453a64077a51c6db5435b7bd9df615ebc06f5199
TamraPeterson <peterson.tamra@gmail.com>
2022-02-25 16:54:05 -0700

    end of class

---
+++ app/Controllers/TodosController.js
@@ -0,0 +1,25 @@
+
+
+// get todos from api
+// draw todos from api
+// add new todo to array
+// draw new todo
+// delete todo from screen and api
+
+function _getTodos() {
+
+}
+
+function _drawTodos() {
+
+}
+
+function _addTodo() {
+
+}
+
+export class TodosController {
+  constructor() {
+
+  }
+}
